c08f0ec587bd9bb5ecaa70c9f06aebdf6ba54fa3
josuetc94 <alu0100763492@ull.edu.es>
2016-10-13 13:53:30 +0000

    Prueba con sendFile

--- app.js
+++ app.js
@@ -8,13 +8,13 @@ var app = express();
 // app.set('views', path.join(__dirname, 'views'));
 // app.set('view engine', 'jade');
 
-app.use(express.static(path.join(__dirname, 'gh-pages')));
+app.use(express.static('gh-pages'));
 
 
 app.set('port', (process.env.PORT || 8080));
  
 app.get('/', function(request, response){
-  response.send('index');
+  response.sendFile('index');
 });
 
 
